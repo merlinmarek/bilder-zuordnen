ab4f95b8acfe61d0c7c9c9c2dbfe5bfc62878803
Merlin Marek <merlin.marek@posteo.net>
2023-09-09 15:39:32 +0200

    fix(ci): set correct base path for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-})
+  base: mode === "production" ? "/bilder-zuordnen/" : "",
+}))
